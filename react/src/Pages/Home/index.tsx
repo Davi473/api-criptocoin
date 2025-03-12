ada29616b31deebda28f02f56a825137aebf6b99
Davi473 <68740118+Davi473@users.noreply.github.com>
2025-03-12 15:03:17 +0000

    init react

---
+++ react/src/Pages/Home/index.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function Home() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div>
+            <p>Ola</p>
+        </div>
+    </>
+  )
+}
+
+export default Home
