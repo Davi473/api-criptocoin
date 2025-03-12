ffd7808f7f37785f91615c0f1ec14a20b495fec0
Davi473 <68740118+Davi473@users.noreply.github.com>
2025-03-12 17:01:15 +0000

    pag home

--- react/src/Pages/Home/index.tsx
+++ react/src/Pages/Home/index.tsx
@@ -1,13 +1,52 @@
 // import { useState } from 'react'
+import styles from "./style"
 
 function Home() {
 //   const [count, setCount] = useState(0)
 
   return (
     <>
-        <div>
-            <p>Ola</p>
+      <div>
+          <h3 style={{fontSize: 20}}>
+            <p style={{fontSize: 15}}>Seja Bem Vindo,</p>
+            Davi
+          </h3>
+      </div>
+      <div style={styles.scrollContainer}>
+        <div style={styles.card}>
+          <div style={styles.cardHeader}>
+            <h2 style={styles.cardTitle}>Test</h2>
+            <p style={{...styles.cardIcone}}>icone</p>  
+          </div>
+          <div style={styles.cardBody}>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+          </div>
         </div>
+        <div style={styles.card}>
+          <div style={styles.cardHeader}>
+            <h2 style={styles.cardTitle}>Test</h2>
+            <p style={{...styles.cardIcone}}>icone</p>  
+          </div>
+          <div style={styles.cardBody}>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+          </div>
+        </div>
+        <div style={styles.card}>
+          <div style={styles.cardHeader}>
+            <h2 style={styles.cardTitle}>Test</h2>
+            <p style={{...styles.cardIcone}}>icone</p>  
+          </div>
+          <div style={styles.cardBody}>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+            <p style={styles.cardText}>Compra: <small style={styles.cardValue}>23</small></p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ react/src/Pages/Home/style.ts
@@ -0,0 +1,39 @@
+const styles = {
+    card: {
+        backgroundColor: '#fff',
+        width: "15%",
+        borderRadius: 8,
+        padding: 16,
+        marginBottom: 15,
+        border: "black solid 1px" 
+    }, 
+    cardBody: {
+        marginBottom: 15
+    },
+    cardHeader: {
+        display: "flex", 
+        justifyContent: "space-between", 
+        alignItems: "center"
+    },
+    cardTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    }, 
+    cardIcone: {
+        fontSize: 20,
+        color: '#555',
+    },
+    cardText: {
+        fontSize: 18
+    },
+    cardValue: {
+        fontSize: 16
+    },
+    scrollContainer: {
+        width: "20%%",
+        overflowX: "auto", 
+        border: "1px solid #ccc",
+        padding: "10px",
+    }
+}
+export default styles
